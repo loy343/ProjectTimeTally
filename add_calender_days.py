b0f2c1802854f1537d520b94fbf412b6db0a3932
loy343 <cute_as-ducks@yahoo.com>
2023-10-19 00:13:51 -0500

    first commit

---
+++ add_calender_days.py
@@ -0,0 +1,30 @@
+import calendar
+import csv
+from datetime import datetime
+
+# Get the current year and month
+current_date = datetime.now()
+year = current_date.year
+month = current_date.month
+
+# Create a list of day numbers and their corresponding day names
+days_in_month = [
+    (day, calendar.day_name[calendar.weekday(year, month, day)])
+    for day in range(1, calendar.monthrange(year, month)[1] + 1)
+]
+
+# Define the output CSV file
+output_file = f'days_of_{current_date.strftime("%B")}_{current_date.strftime("%y")}.csv'
+
+# Write the data to a CSV file
+with open(output_file, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    
+    # Write the CSV header
+    csvwriter.writerow(['Day', 'Day of Week', 'Hours'])
+    
+    # Write the day and day of the week for each day in the month
+    for day, day_of_week in days_in_month:
+        csvwriter.writerow([day, day_of_week])
+
+print(f"CSV file '{output_file}' created with days and their corresponding day of the week for the current month.")
---
+++ hours.py
@@ -0,0 +1,63 @@
+# import calendar, datetime
+
+# year=2023
+
+
+# calendar.Calendar(firstweekday=0)
+# calendar.setfirstweekday(calendar.WEDNESDAY)
+
+# date = datetime.datetime.now()
+
+# print(date)
+# print(calendar.firstweekday())
+
+# print(calendar.month(2023, 10))
+
+
+import datetime
+
+# Initialize an empty dictionary to store hours worked by week
+weekly_hours = {}
+current_week_start = None
+
+while True:
+    try:
+        # Prompt for the date worked (in the format YYYY-MM-DD)
+        #date_str = input("Enter the date worked (YY-MM-DD): ")
+        now = datetime.date.today()
+        year = (now.year)
+        month = (now.month)
+        day = (now.day)
+        date =f"{year}-{month}-{day}"
+        # Convert the date input to a datetime object
+        date_str = date
+        date_worked = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
+        print(date_worked)
+        # Find the next Wednesday from the given date
+        while date_worked.weekday() != 2:  # Wednesday is 2 in Python's date.weekday()
+            date_worked += datetime.timedelta(days=1)
+
+        # Check if the current week has changed
+        if date_worked != current_week_start:
+            if current_week_start is not None:
+                total_hours = sum(weekly_hours.values())
+                print("Weekly Summary for {} to {}: {:.2f} hours".format(current_week_start, date_worked - datetime.timedelta(days=1), total_hours))
+            current_week_start = date_worked
+            weekly_hours = {}
+
+        # Prompt for the hours worked on that day
+        hours_worked = float(input("Enter hours worked on {}: ".format(date_worked)))
+
+        # Add the hours worked to the dictionary
+        weekly_hours[date_worked] = weekly_hours.get(date_worked, 0) + hours_worked
+
+        another_entry = input("Do you want to add more hours? (yes/no): ").lower()
+        if another_entry != "yes":
+            break
+    except (ValueError, KeyError):
+        print("Invalid input. Please try again.")
+
+# Calculate and display the last week's summary
+total_hours = sum(weekly_hours.values())
+print("Weekly Summary for {} to {}: {:.2f} hours".format(current_week_start, date_worked, total_hours))
+
